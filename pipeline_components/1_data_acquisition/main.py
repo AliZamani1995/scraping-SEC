e5698161c0dff13e6fda45167d22b18ba8646448
Ali <azamani1@ualberta.ca>
2023-02-24 02:02:46 -0700

    complete 1_data_acquisition/main.py

--- pipeline_components/1_data_acquisition/main.py
+++ pipeline_components/1_data_acquisition/main.py
@@ -2,11 +2,13 @@
 The file should implement data acquisition pipeline.
 It should load the raw data from any data source and save it.
 """
+import warnings
 from typing import List
 
-from src.utils import Log, load_yaml, parse_arguments, write_parquet
 from src.data.acquisition.utils import load_data
+from src.utils import Log, load_yaml, parse_arguments, write_parquet
 
+warnings.filterwarnings('ignore')
 
 
 def main(arguments_list: List = None):
@@ -15,14 +17,19 @@ def main(arguments_list: List = None):
     config = load_yaml(arguments.config_path)
     main_config = load_yaml(arguments.main_config_path)
 
-    Log.info(main_config.log_msg.acquire_data_start_msg)
-
     Log.info(main_config.log_msg.load_dataset_start_msg)
-    dataset_df = load_data(target_column=main_config.target_column)
+
+    data_df = load_data(
+        cik_dict=config.cik,
+        base_url=config.base_url,
+        extend_url=config.extend_url,
+        headers=config.headers
+    )
+
     Log.info(main_config.log_msg.load_dataset_end_msg)
 
     Log.info(main_config.log_msg.save_dataset_start_msg)
-    write_parquet(df=dataset_df, path=main_config.path.raw_data_path)
+    write_parquet(df=data_df, path=main_config.path.raw_data_path)
     Log.info(main_config.log_msg.save_dataset_end_msg)
 
     Log.info(main_config.log_msg.acquire_data_end_msg)
